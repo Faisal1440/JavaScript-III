a01472f15e528f592b71e653e4652bf418ec68ca
Faisal Aziz <faziz6006@gmail.com>
2019-10-17 14:44:37 -0500

    final sub

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -7,7 +7,7 @@
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
@@ -15,6 +15,15 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+function GameObject(attributes){
+  this.createdAt = attributes.createdAt;
+  this.name = attributes.name;
+  this.dimensions = attributes.dimensions
+}
+
+GameObject.prototype.destroy = function() {
+    return `${this.name} was removed from the game.`
+};
 
 /*
   === CharacterStats ===
@@ -23,6 +32,16 @@
   * should inherit destroy() from GameObject's prototype
 */
 
+function CharacterStats(attri) {
+  GameObject.call(this, attri);
+  this.healthPoints = attri.healthPoints
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`
+}
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
   * team
@@ -33,6 +52,20 @@
   * should inherit takeDamage() from CharacterStats
 */
  
+function Humanoid(attrs){
+  GameObject.call(this, attrs);
+   CharacterStats.call(this, attrs);
+   this.team = attrs.team;
+   this.weapons = attrs.weapons;
+   this.language = attrs.language;
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+
+Humanoid.prototype.greet = function() {
+ return `${this.name} offers a greeting in ${this.language}.`;
+ }
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +74,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,11 +135,9 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
-
-  First sub
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,12 @@
-/* The for principles of "this";
+/* The for principles of "this" ; (reference an object without having to refer to that object’s name.
+Overview)
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding: if u dont have varable to refer to , "this will select the global object (everything in html)"
+* 2. Implicit Binding : inside an object it has key and the this key gives me the value of {object name)
+* 3. New binding: inside of constructuer funced used to refer to the child inside a parent ( make new object)
+* 4. Explicit binding: 
+
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +15,61 @@
 
 // code example for Window Binding
 
+function myschool(name){
+    console.log(this);
+    return "wellcome to: " + name;
+  }
+ 
+  myschool("Lambda");
+
 // Principle 2
 
 // code example for Implicit Binding
 
-// Principle 3
+const student = {
+    schoolname: 'Lambda School',
+    greeting: function(name){
+      console.log(`Wellcome ${name} to ${this.schoolname} ..!`);
+      console.log(this)
+  
+    }
+  }
+  student.greeting('Faisal');
+
+// Principle 3 (con fun)
 
 // code example for New Binding
+
+function student3(name) {
+    this.name = name;
+    this.schoolname = "Lambda School";
+    this.great = function() {
+      console.log(`Hello ${this.name} for your first day in ${this.schoolname}`);
+      console.log(this)
+    }
+  } 
+  const studentName = new student3("Faisal")
+  studentName.great();
 
 // Principle 4
 
 // code example for Explicit Binding
+
+function student4(name) {
+    this.name = name;
+    this.schoolname = "Lambda School";
+    this.great = function() {
+      console.log(`Hello ${this.name} for your first day in ${this.schoolname}`);
+      console.log(this)
+    }
+  } 
+  const studentName1 = new student4("Faisal")
+  studentName.great();
+
+  
+  const studentName2 = new student4("Vick")
+  studentName2.great();
+
+
+  studentName1.great.call(studentName2);
+  studentName2.great.call(studentName1)
